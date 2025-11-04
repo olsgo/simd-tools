12dd66088448b3236bd9ffbaae4f358693d6ccd5
olsgo <72028144+olsgo@users.noreply.github.com>
2025-11-04 13:21:51 -0300

    local patches for rothko

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", .upToNextMajor(from: "600.0.1")),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "602.0.0"),
     ],
     targets: [
         .macro(
